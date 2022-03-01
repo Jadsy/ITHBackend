5158f03f057d667e28acc1e25858c9abb1169bd1
Jadsy <jadsaad01@gmail.com>
2022-03-01 15:56:16 +0200

    fix

--- main/models.py
+++ main/models.py
@@ -24,7 +24,7 @@ class Profile(models.Model):
     created = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return self.id
+        return self.user.username
 
 
 class Project(models.Model):
